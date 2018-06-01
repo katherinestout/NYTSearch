5dddc054601f4f9c325ccee93d9be20ff4ef9327
nchenari <nimachenari@gmail.com>
2018-06-01 14:17:21 -0400

    added jQuery val extraction

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,24 @@
 
-var query = "friday";
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=e7a9301ff4a845c0bad25f903a300d6e";
 
 
-$.ajax({
-  url: queryUrl,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+$("#searchBtn").on("click", function() {
+
+    var query = $("#search-input").val();
+    var numRecords = $("#num-records-input").val();
+
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "?page=" + numRecords + "&api-key=e7a9301ff4a845c0bad25f903a300d6e";
+    console.log("query url: " + queryUrl);
+    
+    $.ajax({
+        url: queryUrl,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+        
+    
+    }).fail(function(err) {
+        throw err;
+    });
+
+})
+
